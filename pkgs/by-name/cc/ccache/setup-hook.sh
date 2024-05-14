43c26b873c54337bed293798a3e7981e61383a81
Radoslaw Sniezek <radoslaw.sniezek@proton.me>
2024-05-14 23:23:37 +0200

    test
    to be dropped

--- pkgs/by-name/cc/ccache/setup-hook.sh
+++ pkgs/by-name/cc/ccache/setup-hook.sh
@@ -1,4 +1,7 @@
+
+
 if [ -z "${dontUseCCacheLauncher-}" ]; then
     cmakeFlags="-DCMAKE_C_COMPILER_LAUNCHER=ccache $cmakeFlags"
     cmakeFlags="-DCMAKE_CXX_COMPILER_LAUNCHER=ccache $cmakeFlags"
+    export TEST_IF_HOOK_EXECUTED=success
 fi
